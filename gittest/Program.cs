c702897d46cfe16b413b1ebdeae04db69588357f
moczardani <moczar.daniel.17f@szlgbp.hu>
2022-01-03 09:54:27 +0100

    uj sor

--- gittest/Program.cs
+++ gittest/Program.cs
@@ -7,6 +7,8 @@ namespace gittest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Új sor");
+            Console.WriteLine("Még egy sor");
         }
     }
 }
